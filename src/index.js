126d31e8852df357c6a0d16a08e531da8251f1f9
AVKostin <alexander.kostin.ua@gmail.com>
2022-02-20 22:23:23 +0200

    make index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,68 @@
 import './css/styles.css';
-// test changes
+import Notiflix from 'notiflix';
+import markupCountryListTemplate from './hbs-templates/markup-country-list.hbs';
+import markupCountryInfoTemplate from './hbs-templates/markup-country-info.hbs';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+
+const searchInput = document.querySelector('#search-box');
+const countriesList = document.querySelector('.country-list');
+const countriesInfo = document.querySelector('.country-info');
 const DEBOUNCE_DELAY = 300;
+
+searchInput.focus();
+
+Notiflix.Notify.init({
+	position: 'center-top',
+	width: '400px',
+	fontSize: '18px',
+});
+
+let countryName = '';
+
+const onSearch = e => {
+	countryName = e.target.value.trim();
+
+	clearInput();
+	setCountries(countryName);
+	function setCountries(countryName) {
+		fetchCountries(countryName)
+			.then(data => {
+				const amount = data.length;
+				console.log(amount);
+				if (amount > 10) {
+					Notiflix.Notify.info(
+						'Найдено очень много стран. Пожалуйста, укажите более конкретное название.',
+					);
+					return;
+				}
+				if (amount >= 2 && amount <= 10) {
+					renderCountriesMarkup(data);
+				} else {
+					renderCountriesInfoMarkup(data);
+				}
+			})
+			.catch(onFetchError);
+	}
+};
+
+function onFetchError(error) {
+	if (countryName !== '') {
+		Notiflix.Notify.failure('Oops, нет страны с таким именем');
+	}
+}
+
+function renderCountriesMarkup(data) {
+	countriesList.insertAdjacentHTML('beforeend', markupCountryListTemplate(data));
+}
+
+function renderCountriesInfoMarkup(data) {
+	countriesInfo.insertAdjacentHTML('beforeend', markupCountryInfoTemplate(data));
+}
+
+function clearInput() {
+	countriesList.innerHTML = '';
+	countriesInfo.innerHTML = '';
+}
+
+searchInput.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
